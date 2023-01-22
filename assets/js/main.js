6c2b88fe51e42bc06df7ee55adff0c22d0b42e77
monukumar <archive1000bc@gmail.com>
2023-01-22 21:22:24 +0530

    compres all file

--- assets/js/main.js
+++ assets/js/main.js
@@ -305,8 +305,17 @@
     });
     
     
+    function onReady(callback) {
+        var intervalID = window.setInterval(checkReady, 1000);
+        function checkReady() {
+            if (document.getElementsByTagName('body')[0] !== undefined) {
+                window.clearInterval(intervalID);
+                callback.call(this);
+            }
+        }
+    }
     
-    
+
 })(jQuery);    
     
 
